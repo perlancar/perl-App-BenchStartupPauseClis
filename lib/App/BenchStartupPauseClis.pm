dfe863b1f571e9aaeffc9afc11867e8c23638be3
perlancar (@pc-office) <perlancar@gmail.com>
2015-07-09 17:42:13 +0700

    Initial commit

---
+++ lib/App/BenchStartupPauseClis.pm
@@ -0,0 +1,13 @@
+package App::BenchStartupPauseClis;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Benchmark startup time of pause CLI's
+
+=head1 SYNOPSIS
+
+See the included scripts:
+
+#INSERT_EXECS_LIST
